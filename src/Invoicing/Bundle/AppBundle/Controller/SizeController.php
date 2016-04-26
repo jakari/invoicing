16f5d530727876e2ff338bc754088236bd4c9435
Jani Kärenaho <jani.karenaho@w3.fi>
2016-04-26 21:10:15 +0300

    Login and root page are now handled by symfony

--- src/Invoicing/Bundle/AppBundle/Controller/SizeController.php
+++ src/Invoicing/Bundle/AppBundle/Controller/SizeController.php
@@ -8,16 +8,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class LoginController extends Controller
+class SizeController extends Controller
 {
     /**
      * @Route("/login", name="login")
      * @Template("@InvoicingApp/login.html.twig")
+     *
+     * @return array
      */
     public function loginAction(Request $request)
     {
         $authenticationUtils = $this->get('security.authentication_utils');
 
         return ['error' => $authenticationUtils->getLastAuthenticationError()];
+    }
+
+    /**
+     * @Route("/", name="site_indez")
+     * @Template("@InvoicingApp/index.html.twig")
+     *
+     * @return array
+     */
+    public function indexAction()
+    {
+        return [];
     }
 }
