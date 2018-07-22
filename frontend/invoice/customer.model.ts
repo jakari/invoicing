71847a7bdcf6830fa855f5c07189e5d20748b68c
Jani Kärenaho <jani@Janis-MBP.lan>
2018-07-22 23:09:03 +0300

    Remove Angular2 frontend code.

--- frontend/invoice/customer.model.ts
+++
@@ -1,9 +0,0 @@
-
-export class CustomerModel {
-    public id:number;
-    public name:string = "";
-    public streetName:string = "";
-    public postCode:string = "";
-    public city:string = "";
-    public email:string = "";
-}
--- frontend/invoice/invoice.list.item.model.ts
+++
@@ -1,9 +0,0 @@
-export class InvoiceListItem {
-    public id:number;
-    public customer:String;
-    public referenceNumber:number;
-    public invoiceNumber:number;
-    public created:Date;
-    public due:Date;
-    public total:number;
-}
